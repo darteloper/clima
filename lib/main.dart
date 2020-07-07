f0b7acf8f618aeb25a2a5182bd95e587234ad071
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-07-06 23:49:43 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Clima());
+
+class Clima extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+    );
+  }
+}
