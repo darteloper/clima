798328d2c3ac8b8818ebcab65eb4ee397730af5f
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-07-20 23:57:52 -0400

    Define app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:clima/loading_screen.dart';
 
-void main() => runApp(Clima());
+void main() => runApp(MyApp());
 
-class Clima extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
