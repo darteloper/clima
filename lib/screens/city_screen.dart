bcb13ffe1ee6518c9db1682ac5466076a03bfc25
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-07-27 21:08:25 -0400

    Add city_screen.dart

---
+++ lib/screens/city_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CityScreen extends StatefulWidget {
+  @override
+  _CityScreenState createState() => _CityScreenState();
+}
+
+class _CityScreenState extends State<CityScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
