d417bd2a7c2b65893d6739f0d5d9dbb76791b7b8
Brayan Mamani <64350375+darteloper@users.noreply.github.com>
2020-09-10 06:21:01 -1200

    Upgrade scartch.dart

--- scartch.dart
+++ scartch.dart
@@ -4,30 +4,28 @@ void main() {
   performTasks();
 }
 
-void performTasks() async {
+Future performTasks() async {
   task1();
-  print(task2());
+  String task2Result = await task2();
+  task3(task2Result);
 }
 
 void task1() {
-  String result = 'task 1 data';
   print('Task 1 complete');
 }
 
-Future<String> task2() async {
-  Duration threeSeconds = Duration(seconds: 3);
+Future task2() async {
+  Duration duration = new Duration(milliseconds: 3000);
 
   String result;
 
-  Future.delayed(threeSeconds, () {
+  await Future.delayed(duration, () {
     result = 'task 2 data';
     print('Task 2 complete');
   });
-
   return result;
 }
 
-void task3(String task2Data) {
-  String result = 'task 3 data';
-  print('Task 3 complete with $task2Data');
+void task3(String task) {
+  print('Task 3 complete $task');
 }
